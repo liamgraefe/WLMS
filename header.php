65e5e8eec29f9ba7fb7d0d2e578d2401f7d2f951
Liam <liam.graefe@iu-study.org>
2024-11-08 12:40:22 +0100

    some modal changes

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
+    <title>WLMS</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link href="style.css" rel="stylesheet">
--- index.php
+++ index.php
@@ -1,110 +1,123 @@
-<?php include("header.php"); ?>
-<?php include("dbcon.php"); ?>
-<div class="box1">
-    <h2>Bookmarks</h2>
-    <button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Add Bookmark</button>
-</div>
-<table class="table table-hover table-bordered table-striped">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>URL</th>
-            <th>Description</th>
-            <th>Category</th>
-            <th>Edit</th>
-            <th>Delete</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
+    <?php include("header.php"); ?>
+    <?php include("dbcon.php"); ?>
 
-        $query = "SELECT * FROM bookmarks";
 
-        $result = mysqli_query($connection, $query);
+    <div class="box1">
+        <h2>Bookmarks</h2>
+        <button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Add Bookmark</button>
+    </div>
+    <table class="table table-hover table-bordered table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>URL</th>
+                <th>Description</th>
+                <th>Category</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
 
-        if (!$result) {
-            die("Query failed" . mysqli_error($connection));
-        } else {
-            while ($row = mysqli_fetch_assoc($result)) {
-        ?>
-                <tr>
-                    <td><?php echo $row['id']; ?></td>
-                    <td><?php echo $row['name']; ?></td>
-                    <td><?php echo $row['url']; ?></td>
-                    <td><?php echo $row['description']; ?></td>
-                    <td><?php echo $row['category_id']; ?></td>
-                    <td><a href="edit.php?id=<?php echo $row['id']; ?>" class="btn btn-success">Edit</a></td>
-                    <td><a href="delete.php?id=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a></td>
-                </tr>
+            $query = "SELECT * FROM bookmarks";
 
-        <?php
+            $result = mysqli_query($connection, $query);
 
+            if (!$result) {
+                die("Query failed" . mysqli_error($connection));
+            } else {
+                while ($row = mysqli_fetch_assoc($result)) {
+            ?>
+                    <tr>
+                        <td><?php echo $row['id']; ?></td>
+                        <td><?php echo $row['name']; ?></td>
+                        <td><a href="url"><?php echo $row['url']; ?></a></td>
+                        <td><?php echo $row['description']; ?></td>
+                        <td><?php echo $row['category_id']; ?></td>
+                        <td><a href="edit.php?id=<?php echo $row['id']; ?>" class="btn btn-success">Edit</a></td>
+                        <td><a href="delete.php?id=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a></td>
+                    </tr>
+
+            <?php
+
+                }
             }
-        }
 
-        ?>
-    </tbody>
-</table>
+            ?>
+        </tbody>
+    </table>
 
-<?php
+    <?php
     if (isset($_GET['message'])) {
         echo "<h6>" . $_GET['message'] . "</h6>";
     }
-?>
-<?php
+    ?>
+    <?php
     if (isset($_GET['insert_msg'])) {
         echo "<h6>" . $_GET['insert_msg'] . "</h6>";
     }
-?>
-<?php
+    ?>
+    <?php
     if (isset($_GET['update_msg'])) {
         echo "<h6>" . $_GET['update_msg'] . "</h6>";
     }
-?>
-<?php
+    ?>
+    <?php
     if (isset($_GET['delete_msg'])) {
         echo "<h6>" . $_GET['delete_msg'] . "</h6>";
     }
-?>
+    ?>
 
-<!-- Modal -->
-<form action="insert_data.php" method="POST">
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">ADD BOOKMARK</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
+    <!-- Modal -->
+    <form action="insert_data.php" method="POST">
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">ADD BOOKMARK</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
 
-                    <div class="form-group">
-                        <label for="name">Name</label>
-                        <input type="text" name="name" class="form-control">
+                        <div class="form-group">
+                            <label for="name">Name
+                            <span class="text-danger">*</span>
+                            </label>
+                            <input type="text" name="name" class="form-control" required maxlength="60">
+                        </div>
+                        <div class="form-group">
+                            <label for="url">URL
+                            <span class="text-danger">*</span>
+                            </label>
+                            <input type="text" name="url" class="form-control" pattern="https?://.*" placeholder="https://example.com" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="description">Description</label>
+                            <input type="text" name="description" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <!-- <label for="category">Category</label>
+                            <input type="number" name="category" class="form-control"> -->
+                            <label for="category" class="form-label">Pick a category:</label>
+                            <select class="form-select" id="dropdown" name="category">
+                                <option value="0">None</option>
+                                <option value="1">Entertainment</option>
+                                <option value="2">Social Media</option>
+                                <option value="3">Shopping</option>
+                            </select>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for="url">URL</label>
-                        <input type="text" name="url" class="form-control">
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <input type="submit" class="btn btn-success" name="add_bookmark" value="ADD">
                     </div>
-                    <div class="form-group">
-                        <label for="description">Description</label>
-                        <input type="text" name="description" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="category">Category</label>
-                        <input type="number" name="category" class="form-control">
-                    </div>
-
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <input type="submit" class="btn btn-success" name="add_bookmark" value="ADD">
                 </div>
             </div>
         </div>
-    </div>
-</form>
-<?php include("footer.php"); ?>
+    </form>
+    <?php include("footer.php"); ?>
+
