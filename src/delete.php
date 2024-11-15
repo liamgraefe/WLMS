0a1f6ab7d65b97cf30ed86ad7d8d193ef7c93f93
Liam <liam.graefe@iu-study.org>
2024-11-15 11:57:29 +0100

    delete old files

--- src/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$data = json_decode(file_get_contents("php://input"), true);
-$id = $data['id'];
-
-$_SESSION['bookmarks'] = array_filter($_SESSION['bookmarks'], function ($bookmark) use ($id) {
-    return $bookmark['id'] !== $id;
-});
-echo json_encode(["status" => "success", "message" => "Lesezeichen gelöscht"]);
--- src/read.php
+++
@@ -1,6 +0,0 @@
-<?php
-//session_start();
-header('Content-Type: application/json');
-
-// Gibt alle gespeicherten Lesezeichen zurück
-echo json_encode($_SESSION['bookmarks'] ?? []);
